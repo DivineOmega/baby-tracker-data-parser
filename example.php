32d26d39527442f3dc9c30883f35f5ef7ba6d695
Jordan Hall <jordan@hall05.co.uk>
2018-12-05 23:03:05 +0000

    Remove debug output

--- example.php
+++ example.php
@@ -6,8 +6,4 @@ $parser = new DivineOmega\BabyTrackerDataParser\Parser(getenv('PATH_TO_ZIP_FILE'
 
 $records = $parser->parseBabyRecords();
 
-foreach($records as $record) {
-    if ($record instanceof \DivineOmega\BabyTrackerDataParser\BabyRecords\DiaperingRecord) {
-        print_r($record);
-    }
-}
+var_dump($records);
